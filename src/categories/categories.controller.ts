a03a024f4c1bb6024a60f2de096cf3d19b87a6d2
joaquinxtx <thejotex1998@gmail.com>
2023-07-06 23:37:16 -0300

    Controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,9 @@
 import {
   Body,
   Controller,
+  Delete,
   FileTypeValidator,
+  Get,
   MaxFileSizeValidator,
   Param,
   ParseFilePipe,
@@ -19,26 +21,74 @@ import { JwtRole } from 'src/auth/jwt/jwt-role';
 import { JwtRolesGuard } from 'src/auth/jwt/jwt-roles.guard';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto copy';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private CategoriesService: CategoriesService) {}
 
+  @HasRoles(JwtRole.ADMIN, JwtRole.CLIENT)
+  @UseGuards(JwtAuthGuard, JwtRolesGuard)
+  @Get()
+  findAll() {
+    return this.CategoriesService.findAll();
+  }
+
   @HasRoles(JwtRole.ADMIN)
   @UseGuards(JwtAuthGuard, JwtRolesGuard)
-  @Post() 
+  @Post()
   @UseInterceptors(FileInterceptor('file'))
   createWithImage(
-      @UploadedFile(
-          new ParseFilePipe({
-              validators: [
-                new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 10 }),
-                new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
-              ],
-            }),
-      ) file: Express.Multer.File,
-      @Body() category: CreateCategoryDto
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 10 }),
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() category: CreateCategoryDto,
   ) {
-      return this.CategoriesService.create(file,category);
+    return this.CategoriesService.create(file, category);
+  }
+  @HasRoles(JwtRole.ADMIN)
+  @UseGuards(JwtAuthGuard, JwtRolesGuard)
+  @Put(":id")
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() category: UpdateCategoryDto,
+  ) {
+    return this.CategoriesService.update(id, category);
+  }
+
+  @HasRoles(JwtRole.ADMIN)
+  @UseGuards(JwtAuthGuard, JwtRolesGuard)
+  @Put("upload/:id")
+  @UseInterceptors(FileInterceptor('file'))
+  updateWithImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 10 }),
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() category: UpdateCategoryDto,
+  ) {
+    return this.CategoriesService.updateWithImage(file, id, category);
+  }
+
+  @HasRoles(JwtRole.ADMIN)
+  @UseGuards(JwtAuthGuard, JwtRolesGuard)
+  @Delete(":id")
+  delete(
+    @Param('id', ParseIntPipe) id: number,
+    
+  ) {
+    return this.CategoriesService.delete(id);
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -4,6 +4,7 @@ import storage =require("../utils/cloud_storage")
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './category.entity';
 import { Repository } from 'typeorm';
+import { UpdateCategoryDto } from './dto/update-category.dto copy';
 
 @Injectable()
 export class CategoriesService {
@@ -11,6 +12,10 @@ export class CategoriesService {
     constructor(
         @InjectRepository(Category) private categoriesRepository: Repository<Category>
     ){}
+
+    findAll(){
+        return this.categoriesRepository.find()
+    }
 
     async create(file:Express.Multer.File, category:CreateCategoryDto){
         
@@ -22,6 +27,40 @@ export class CategoriesService {
         const newCategory = this.categoriesRepository.create(category)
 
         return this.categoriesRepository.save(newCategory)
+    }
+    async updateWithImage(file:Express.Multer.File,id:number, category:UpdateCategoryDto){
+        
+        const url = await storage(file, file.originalname)
+        if(url === undefined && url === null ){
+            throw new HttpException("La imagen no se pudo guardar" , HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+        const categoryFound = await this.categoriesRepository.findOneBy({id:id})
+        if(!categoryFound){
+            throw new HttpException("La categoria no existe" , HttpStatus.NOT_FOUND)
+        }
+        category.image = url
+        const updatedCategory = Object.assign(categoryFound, category)
+
+        return this.categoriesRepository.save(updatedCategory)
+    }
+    async update(id:number, category:UpdateCategoryDto){
+    
+        const categoryFound = await this.categoriesRepository.findOneBy({id:id})
+        if(!categoryFound){
+            throw new HttpException("La categoria no existe" , HttpStatus.NOT_FOUND)
+        }
+      
+        const updatedCategory = Object.assign(categoryFound, category)
+
+        return this.categoriesRepository.save(updatedCategory)
+    }
+
+    async delete(id:number){
+        const categoryFound = await this.categoriesRepository.findOneBy({id:id})
+        if(!categoryFound){
+            throw new HttpException("La categoria no existe" , HttpStatus.NOT_FOUND)
+        }
+        return this.categoriesRepository.delete(id)
     }
 
 
