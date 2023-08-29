7e71bfd9bbd23d82e5a65f7b315eba5f115e64e0
joaquinxtx <thejotex1998@gmail.com>
2023-08-28 21:58:26 -0300

    Visa/mastercard cuotes

--- src/mercado_pago/mercado_pago.controller.ts
+++ src/mercado_pago/mercado_pago.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { MercadoPagoService } from './mercado_pago.service';
 import { JwtRole } from 'src/auth/jwt/jwt-role';
 import { HasRoles } from 'src/auth/jwt/has-roles';
@@ -7,14 +13,22 @@ import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 
 @Controller('mercadopago')
 export class MercadoPagoController {
-    constructor(private mercadoPagoService:MercadoPagoService){}
+  constructor(private mercadoPagoService: MercadoPagoService) {}
 
-    @HasRoles(JwtRole.ADMIN,JwtRole.CLIENT)
+  @HasRoles(JwtRole.ADMIN, JwtRole.CLIENT)
+  @UseGuards(JwtAuthGuard, JwtRolesGuard)
+  @Get('identification_types')
+  getIdentificationTypes() {
+    return this.mercadoPagoService.getIdentificationTypes();
+  }
 
-    @UseGuards(JwtAuthGuard, JwtRolesGuard)
-
-    @Get("identification_types")
-    getIdentificationTypes(){
-return this.mercadoPagoService.getIdentificationTypes()
-    }
+  @HasRoles(JwtRole.ADMIN, JwtRole.CLIENT)
+  @UseGuards(JwtAuthGuard, JwtRolesGuard)
+  @Get('installments/:first_six_digits/:amount')
+  getInstallments(
+    @Param('first_six_digits', ParseIntPipe) firstSixtDigits: number,
+    @Param('amount', ParseIntPipe) amount: number,
+  ) {
+    return this.mercadoPagoService.getInstallments(firstSixtDigits,amount);
+  }
 }
--- src/mercado_pago/mercado_pago.service.ts
+++ src/mercado_pago/mercado_pago.service.ts
@@ -4,6 +4,7 @@ import { AxiosResponse  , AxiosError} from 'axios';
 import { Observable , catchError,map} from 'rxjs';
 import { IdentificationType } from './models/identification_type';
 import { HEADERS_MERCADO_PAGO, MERCADO_PAGO_API } from 'src/config/config';
+import { Installment, PayerCost } from './models/Installments';
 
 
 @Injectable()
@@ -18,6 +19,17 @@ export class MercadoPagoService {
             })
         ).pipe(
             map((resp)=> resp.data)
+        )
+
+    }
+
+    getInstallments(firstSixDigits:number , amount:number): Observable<PayerCost[]>{
+        return this.httpService.get(MERCADO_PAGO_API + `/payment_methods/installments?bin=${firstSixDigits}&amount=${amount}`, {headers:HEADERS_MERCADO_PAGO}).pipe(
+            catchError((error:AxiosError) =>{
+            throw new HttpException(error.response.data,error.response.status)
+            })
+        ).pipe(
+            map((resp: AxiosResponse<Installment[]>)=> resp.data[0].payer_costs)
         )
 
     }
